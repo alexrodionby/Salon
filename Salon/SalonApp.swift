b7258cddb268c6cc78aadce2dc99908d86114eb1
Alexandr Rodionov <alexrodionby@gmail.com>
2022-07-30 18:05:35 +0300

    build(app): initialization of the initial project

---
+++ Salon/SalonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalonApp.swift
+//  Salon
+//
+//  Created by Alexandr Rodionov on 30.07.22.
+//
+
+import SwiftUI
+
+@main
+struct SalonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
